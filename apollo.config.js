808f5b0946d75a955a541c63dcecca8e4f757fa3
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-07-02 21:29:26 +0200

    fix: fix package.json and eslint

--- apollo.config.js
+++ apollo.config.js
@@ -1,19 +1,20 @@
+/* eslint-env node */
 require('dotenv').config()
 module.exports = {
   client: {
     includes: [
       './hooks/src/**/*.ts',
       './worker/src/**/*.ts',
-      './frontend/src/**/*.ts',
+      './frontend/src/**/*.ts'
     ],
     service: {
       name: 'hasura',
       // localSchemaFile: './schema.graphql',
       url: 'http://localhost:8080/v1/graphql',
       headers: {
-        'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+        'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET
       },
-      skipSSLValidation: true,
-    },
-  },
+      skipSSLValidation: true
+    }
+  }
 }
