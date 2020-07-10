e0ead3713a43714200e58081de2f0a23430034bd
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-07-10 11:18:19 +0200

    fix(hooks): rabbitmq host

--- hooks/src/config.ts
+++ hooks/src/config.ts
@@ -2,10 +2,11 @@ export const RABBITMQ_DEFAULT_USER =
   process.env.RABBITMQ_DEFAULT_USER || 'guest'
 export const RABBITMQ_DEFAULT_PASS =
   process.env.RABBITMQ_DEFAULT_PASS || 'guest'
+export const RABBITMQ_HOST = process.env.RABBITMQ_HOST || 'rabbitmq'
 
 export const PORT = 3000
 
-export const RABBITMQ_URL = `amqp://${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}@rabbitmq`
+export const RABBITMQ_URL = `amqp://${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}@${RABBITMQ_HOST}`
 export const TILE_SET_QUEUE = 'tile_set_queue'
 
 export const MIN_ZOOM = 1
