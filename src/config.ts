154fd1a15304469dfd5814c88fad73e56a734499
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-07-10 20:55:17 +0200

    fix(frontend): quasar-processed process.env variables always exist -> force type as string

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const HBP_ENDPOINT = process.env.HBP
-export const HASURA_WS_ENDPOINT = process.env.HASURA
+export const HBP_ENDPOINT = process.env.HBP as string
+export const HASURA_WS_ENDPOINT = process.env.HASURA as string
