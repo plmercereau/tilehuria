cad135769bcd1cb980ed5db121ede3a16327f707
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-07-27 16:17:43 +0200

    refactor(frontend): use the built-in quasar file size format

--- src/filters/pretty-bytes.ts
+++ src/filters/pretty-bytes.ts
@@ -1,27 +1,7 @@
 import Vue from 'vue'
+import { format } from 'quasar'
+const { humanStorageSize } = format
 
 Vue.filter('prettyBytes', function(num: number) {
-  // jacked from: https://github.com/sindresorhus/pretty-bytes
-  if (typeof num !== 'number' || isNaN(num)) {
-    throw new TypeError('Expected a number')
-  }
-
-  const neg = num < 0
-  const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-
-  if (neg) {
-    num = -num
-  }
-
-  if (num < 1) {
-    return (neg ? '-' : '') + String(num) + ' B'
-  }
-
-  const exponent = Math.min(
-    Math.floor(Math.log(num) / Math.log(1000)),
-    units.length - 1
-  )
-  const res = (num / Math.pow(1000, exponent)).toFixed(2)
-
-  return (neg ? '-' : '') + res + ' ' + units[exponent]
+  return humanStorageSize(num)
 })
